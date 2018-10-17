6844cc13c1d7d0ec256b31544b2c7461fc61f066
nagane <nagane@naganenoMacBook-puro.local>
2018-10-17 15:53:09 +0900

    try and error

--- bot/bot.js
+++ bot/bot.js
@@ -2,7 +2,7 @@ var botBuilder = require('claudia-bot-builder'),
     excuse = require('huh');
 
 module.exports = botBuilder(function (request) {
-  return 'approoove' + 'Thanks for sending ' + request.text  + 
+  return 'mettyayarinaositeru' + 'Thanks for sending ' + request.text  + 
       '. Your message is very important to us, but ' + 
       excuse.get();
 });
