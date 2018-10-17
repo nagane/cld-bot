0d227064cb42cae171b82737c5bbd430ef6d26f6
nagane <nagane@naganenoMacBook-puro.local>
2018-10-17 11:51:33 +0900

    modify messages

--- bot/bot.js
+++ bot/bot.js
@@ -2,7 +2,7 @@ var botBuilder = require('claudia-bot-builder'),
     excuse = require('huh');
 
 module.exports = botBuilder(function (request) {
-  return 'oini-oini-' + 'Thanks for sending ' + request.text  + 
+  return 'harahetta' + 'Thanks for sending ' + request.text  + 
       '. Your message is very important to us, but ' + 
       excuse.get();
 });
